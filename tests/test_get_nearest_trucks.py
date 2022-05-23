327dc90233139cf35e9f9978f637876ab770ce60
kuan yu <kuan@mview.com.au>
2022-05-23 17:18:55 +1000

    feat: Add more test cases

--- tests/test_get_nearest_trucks.py
+++ tests/test_get_nearest_trucks.py
@@ -9,3 +9,11 @@ trucks = [
 def test_nearest_5():
   results = get_nearest_trucks(trucks, 3.1, -3.1, 5)
   assert [i["Applicant"] for i in results] == ["App3", "App4", "App2", "App5", "App1"]
+
+def test_empty():
+  results = get_nearest_trucks([], 3.1, -3.1, 5)
+  assert results == []
+
+def test_big_num():
+  results = get_nearest_trucks(trucks, 3.1, -3.1, 100)
+  assert len(results) == 10
